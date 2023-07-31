d16e9cd56a1d650de501d1e06b4bd0ff7c3ec4b2
Gabriel Riomar Lopes <gabrielriomar@gmail.com>
2023-07-31 19:08:17 -0300

    adding boostrap and laravel.mix

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,6 +1,10 @@
 <x-layout title="New Serie">
   <form action="" method="post">
-    <label for="name">Name</label>
-    <input type="text" id="name" name="name">
+    <div class="mb-3">
+      <label for="name" class="form-label">Name</label>
+      <input type="text" id="name" name="name" class="form-control">
+    </div>
+    <button type="submit" class="btn btn-success">Submit</button>
+    <button type="button" class="btn btn-danger" onclick="history.back()">Back</button>
   </form>
 </x-layout>
