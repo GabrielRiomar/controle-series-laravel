c931a2567f0b9ff6eb47eb2502625785665364b4
Gabriel Riomar Lopes <gabrielriomar@gmail.com>
2023-08-07 18:43:15 -0300

    adding new routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\EpisodesController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\SeasonsController;
 use App\Http\Controllers\SeriesController;
+use App\Http\Controllers\UsersController;
+use App\Http\Middleware\Authenticator;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,15 +22,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware(Authenticator::class);
 
 Route::resource('/series', SeriesController::class)->except('show');
-// Route::controller(SeriesController::class)->group(function () {
-//     Route::get('/series', 'index')->name('series.index');
-//     Route::get('/series/new', 'create')->name('series.create');
-//     Route::post('/series/save', 'store')->name('series.store');
-// });
-
-// Route::delete('/series/destroy/{serie}', [SeriesController::class, 'destroy'])->name('series.destroy');
 
 Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
+
+Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
+Route::post('/seasons/{season}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'signIn'])->name('signin');
+
+Route::get('/register', [UsersController::class, 'create'])->name('users.create');
+Route::post('/register', [UsersController::class, 'store'])->name('users.store');
